680c013bcc0ff577bc5373e28f3bb2fadf02b20e
sh <vshushkanov@berlogic.de>
2019-08-07 16:34:15 +0200

    fixed thread pool vs cached

--- src/io/server/ExecutorServiceBlockingServer.java
+++ src/io/server/ExecutorServiceBlockingServer.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Executors;
 public class ExecutorServiceBlockingServer {
 
 	public static void main(String[] args) throws IOException {
-		final ExecutorService pool = Executors.newCachedThreadPool();
+		final ExecutorService pool = Executors.newFixedThreadPool(10);
 		final Thread.UncaughtExceptionHandler exHandler = (t, e) -> System.out.println(t + ": " + e);
 
 		final Handler<Socket> handler =
